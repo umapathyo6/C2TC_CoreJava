96955b92844164c0206205e4516a8bd9840050e8
umapathy <umapathy82488@gmail.com>
2025-09-30 11:21:07 +0530

    Frist project in github

---
+++ corejava/src/day1/CoreJava.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class CoreJava {
+
+	public static void main(String[] args) {
+		var b="umapathy";
+		System.out.println(b);
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
